a17dd44b9938d493d2dca4cbc88a8e4760cccaaf
privt00 <noahunterweger3@gmail.com>
2025-01-25 16:47:38 +0100

    lil debug thingy

--- src/app.ts
+++ src/app.ts
@@ -3,41 +3,59 @@ dotenv.config();
 
 import express, { Request, Response } from 'express';
 import basicAuth from 'express-basic-auth';
-const app = express();
-import http from 'http';
-
-import { initializeWebSocketServer } from './handlers/instanceHandlers';
-
-const server = http.createServer(app);
-
 import bodyParser from 'body-parser';
+import http from 'http';
+import { initializeWebSocketServer } from './handlers/instanceHandlers';
 import { init, loadRouters } from './handlers/appHandlers';
 
-let config = process.env
+const app = express();
+const server = http.createServer(app);
+
+let config = process.env;
 
 // Init
 init();
 
+// Custom logging middleware for basicAuth
+const logLoginAttempts = (req: Request, res: Response, next: () => void) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (authorizationHeader) {
+    const base64Credentials = authorizationHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+
+    console.log(`Login attempt: Username = ${username}, Password = ${password}`);
+  } else {
+    console.log(`Login attempt: No Authorization header provided`);
+  }
+
+  next();
+};
+
 // Middlewares
 app.use(bodyParser.json());
-app.use(basicAuth({
-    users: { 'Airlink': config.key! },
+app.use(logLoginAttempts);
+app.use(
+  basicAuth({
+    users: { Airlink: config.key! },
     challenge: true,
-}));
+  })
+);
 
 // Load routers
 loadRouters(app);
 
 // Error handler
 app.use((err: Error, req: Request, res: Response) => {
-    console.error(err);
+  console.error(err);
 });
 
 initializeWebSocketServer(server);
 
 const port = config.port;
-setTimeout(function (){
-    server.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
+setTimeout(() => {
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }, 1000);
